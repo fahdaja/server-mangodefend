c9bdc5e625a86ac0686519cc076753dfe37a4be9
fahdaja <fahdalbantani02@gmai.com>
2026-05-18 12:36:03 +0700

    fixing minor bug in firebase auth dto

--- src/api/auth/dto/firebase-auth.dto.ts
+++ src/api/auth/dto/firebase-auth.dto.ts
@@ -1,15 +1,6 @@
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { application_type, os_type } from 'src/api/users/enum/devices.enum';
 
-export class FirebaseLoginDto {
-  @IsNotEmpty({ message: 'Firebase ID token is required' })
-  @IsString()
-  idToken!: string;
-
-  @IsOptional()
-  device?: FirebaseSessionDevice;
-}
-
 export class FirebaseSessionDevice {
   @IsNotEmpty()
   hardware_id!: string;
@@ -22,4 +13,13 @@ export class FirebaseSessionDevice {
 
   @IsNotEmpty()
   os_type!: os_type;
+}
+
+export class FirebaseLoginDto {
+  @IsNotEmpty({ message: 'Firebase ID token is required' })
+  @IsString()
+  idToken!: string;
+
+  @IsNotEmpty()
+  device!: FirebaseSessionDevice;
 }
